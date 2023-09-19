01b04ef378c4913773f1f51713aa4d5d8b8115cc
Pankaj <pankaj@gmail.com>
2023-09-19 14:58:25 +0530

    Maven project

---
+++ src/main/java/pankaj/automation/Pankaj.java
@@ -0,0 +1,9 @@
+package pankaj.automation;
+
+public class Pankaj
+{
+    public static void main( String[] args )
+    {
+        System.out.println("Name:Pankaj");
+    }
+}
---
+++ src/main/java/pankaj/automation/pank.java
@@ -0,0 +1,9 @@
+package pankaj.automation;
+
+public class pank
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "UID:22MCC20045" );
+    }
+}
